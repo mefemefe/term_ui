904cb43351addd09332c22f364d89dd541adfbf2
Pablo Rosana <50670336+mefemefe@users.noreply.github.com>
2024-04-28 19:53:56 -0300

    improving for mobile...

--- js/main.js
+++ js/main.js
@@ -23,3 +23,15 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
+
+// TODO: do something with this
+window.visualViewport.addEventListener('resize', function() {
+    // Check if the visible height has decreased (indicating keyboard open)
+    if (window.innerHeight > window.visualViewport.height) {
+      // Keyboard is open, add a class to the body
+      document.body.classList.add('keyboard-open');
+    } else {
+      // Keyboard is closed, remove the class
+      document.body.classList.remove('keyboard-open');
+    }
+});
